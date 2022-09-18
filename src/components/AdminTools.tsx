d1d8005b62570f619d619fa71be1cde1fcbee00c
Melissa Rose <123melissa321@gmail.com>
2022-09-18 18:52:05 +0100

    Added admin tools links

--- src/components/AdminTools.tsx
+++ src/components/AdminTools.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 function AdminTools(): JSX.Element {
     return (
-        <p>Admin tools</p>
+        <><p>Admin tools</p>
+        <Link to="/">Home</Link>
+        <Link to="admin-calendar">Calendar</Link>
+        <Link to="admin-covers">Covers</Link>
+        </>
     )
 }
 
