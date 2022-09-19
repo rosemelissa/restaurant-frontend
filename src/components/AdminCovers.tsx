f32000b09a5cb03c9ad8e7f10717812b64e2bfa2
Melissa Rose <123melissa321@gmail.com>
2022-09-19 12:08:10 +0100

    Get covers for next seven days

--- src/components/AdminCovers.tsx
+++ src/components/AdminCovers.tsx
@@ -1,15 +1,34 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { baseUrl } from "../utils/baseUrl";
+import getNextSevenDays from "../utils/getNextSevenDays";
 
 function AdminCovers(): JSX.Element {
     const [date, setDate] = useState<string>(new Date().toISOString().substring(0, 10));
+    // const [sevenDays, setSevenDays] = useState<string[]>(getNextSevenDays(new Date().toISOString().substring(0, 10)))
     const [covers, setCovers] = useState<{date: string, total_covers: number}[]|null>(null)
     useEffect(() => {
         const getCovers = async () => {
             const res = await axios.get(`${baseUrl}/covers/${date}`);
-            setCovers(res.data);
+            const datesWithCovers: {date: string, total_covers: number}[] = res.data;
+            const allDatesCovers: {date: string, total_covers: number}[] = [];
+            const sevenDays = getNextSevenDays(date);
+            console.log('seven days', sevenDays)
+            for (let i=0; i<7; i++) {
+                let dateAdded = false;
+                for (const dateWithCover of datesWithCovers) {
+                    if (!dateAdded && (dateWithCover.date === sevenDays[i])) {
+                        allDatesCovers.push(dateWithCover);
+                        dateAdded = true;
+                    }
+                }
+                if (!dateAdded) {
+                    allDatesCovers.push({date: sevenDays[i], total_covers: 0})
+                }
+            }
+            setCovers(allDatesCovers);
         }
+        // setSevenDays(getNextSevenDays(date))
         getCovers();
     }, [date])
     return (
@@ -21,10 +40,10 @@ function AdminCovers(): JSX.Element {
         {covers &&
             covers.map((cover, i) => {
                 return (
-                    <>
+                    <div key={i}>
                     <p>Date: {cover.date}</p>
                     <p>Number of people: {cover.total_covers}</p>
-                    </>
+                    </div>
                 )
             })
         }
---
+++ src/utils/getNextSevenDays.ts
@@ -0,0 +1,8 @@
+export default function getNextSevenDays(startDate: string): string[] {
+    const sevenDays: string[] = [startDate];
+    for (let i = 1; i < 7; i++) {
+        const dayToAdd: string = (new Date(Date.parse(startDate) + (1000*60*60*24*i))).toISOString().substring(0, 10);
+        sevenDays.push(dayToAdd);
+    }
+    return sevenDays;
+}
