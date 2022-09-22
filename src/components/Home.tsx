2f97818ea7ef8fce276e88e2ec6d394ba9963a9d
Melissa Rose <123melissa321@gmail.com>
2022-09-22 17:59:45 +0100

    Slideshow

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import Slideshow from "./Slideshow";
 
 function Home(): JSX.Element {
     return (
         <div id='home-page'>
             <div id='restaurant-info'>
-            <h1>Tasty restuarant</h1>
+                <div id='restaurant-info-text'><h1>Tasty restuarant</h1>
             <p>Great food, great drinks, great service</p>
-            <Link to="/make-booking" id='make-booking-link' className='green-button'>Make booking</Link>
+            <Link to="/make-booking" id='make-booking-link' className='green-button'>Make booking</Link></div>
+            <div id='restaurant-info-slideshow'><Slideshow /></div>
+            
             </div>
             <Link to="/admin-tools" id='admin-tools-link' className='orange-button'>Admin Tools</Link>
         </div>
